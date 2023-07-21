c8daafb71562f87653d5b5e3652d2153c1b7733b
Evan <suireevan@gmail.com>
2023-07-22 01:10:33 +0200

    rework movement

--- asset/js/game.js
+++ asset/js/game.js
@@ -5,19 +5,38 @@ var posx, posy;
 player.style.top = "500px";
 player.style.left = "1300px";
 
+const step = 25;
 
-document.addEventListener("keydown", function (event) {
-    if(event.key === "z"){
-        posy = player.offsetTop - 25;
-        player.style.top = posy + "px";
-    }else if (event.key === "s"){ 
-        posy = player.offsetTop + 25;
-        player.style.top = posy + "px";
-    }else if (event.key === "q"){
-        posx = player.offsetLeft - 25;
-        player.style.left = posx + "px";
-    }else if (event.key === "d"){
-        posx = player.offsetLeft + 25;
-        player.style.left = posx + "px";
-    }
+const activeKeys = {};
+
+document.addEventListener("keydown", function(event) {
+  activeKeys[event.key] = true;
+  updatePlayerPosition();
 });
+
+document.addEventListener("keyup", function(event) {
+  delete activeKeys[event.key];
+  updatePlayerPosition();
+});
+
+function updatePlayerPosition() {
+  let posx = player.offsetLeft;
+  let posy = player.offsetTop;
+
+  if (activeKeys["z"]) {
+    posy -= step;
+  }
+  if (activeKeys["s"]) {
+    posy += step;
+  }
+  if (activeKeys["q"]) {
+    posx -= step;
+  }
+  if (activeKeys["d"]) {
+    posx += step;
+  }
+
+  // Update the player's position
+  player.style.top = posy + "px";
+  player.style.left = posx + "px";
+}
